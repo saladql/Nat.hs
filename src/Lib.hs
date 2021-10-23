0691d527bf044267854efc81354697ae97185028
Dante Elrik <dante@elrikgroup.com>
2021-10-23 13:24:21 -0400

    FIR-666

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs,LambdaCase,RankNTypes #-}
+{-# LANGUAGE GADTs,LambdaCase,RankNTypes,TypeOperators,PolyKinds,DataKinds #-}
 module Lib where
 import Prelude (Num(..)) 
 import Prelude hiding (succ, fromIntegral)
@@ -29,7 +29,7 @@ instance Eq Nat where
 (>>>) z n = S z n
 
 instance Semigroup Nat where (<>) = (>>>)
-instance Monoid Nat    where mempty = NS (>>>) zero
+instance Monoid Nat    where mempty = NS (>>>) (zero  >>> zero)
 
 -- showsPrec d x r ++ s  ==  showsPrec d x (r ++ s)
 instance Show Nat where 
@@ -60,6 +60,7 @@ data VDiv where
   VDivm :: Nat -> VDiv
   VApp  :: VDiv -> VDiv -> VDiv
   VDiv  :: Nat -> VDiv
+  VDivF :: (VDiv -> VDiv) -> VDiv
 
 (VDivp p) `VApp` (VDivq q) `VApp` (VDivm m) `VApp` (VDiv d)
   | pExhausted p && qExhausted q = VDivm zero
@@ -76,3 +77,12 @@ data VDiv where
                    (NZ _) -> True
                    _     -> False
     balanced = False
+
+newtype Mod       = Mod { runMod :: Nat -> Nat -> Equation Nat Nat Nat Nat  }
+newtype Equals    = Equals { runEquals :: Nat -> Nat -> Mod ->  (Nat `Mod` Nat) `Equals` (Nat,Nat) }
+data Equation p q where
+  Equation :: forall p q m d. (Digit p, Digit q) => () -> (VDivp p) `VApp` (VDivq q) `VApp` (VDivm p) `VApp` (VDiv q)
+
+(r `Mod` q) `Equals` (q,r) :: (VDivp r) `VApp` (VDivq q) `VApp` (VDivm Nat) `VApp` (VDiv q)
+
+someFunc = return ()
