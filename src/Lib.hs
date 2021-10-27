0dfb99a791a4abd16d043ffc9932aabcca5a5278
Dante Elrik <dante@elrikgroup.com>
2021-10-27 02:05:15 -0400

    FIR-666

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,29 +8,42 @@ import Debug.Trace
 type Digit n = (Eq n, Show n) => n 
 type VApp vapp = forall vapp. (vapp -> vapp) -> vapp
 data DNA where
-  EItherDNA :: DNA -> DNA  -> DNA
-  HI        :: DNA -> ()   -> DNA
-  OI        :: ()  -> DNA  -> DNA
-  NI        :: ()  -> ()   -> DNA
-  HHINOI    :: DNA -> ()   -> DNA
-  HOINHI    :: ()  -> DNA  -> DNA
-  NHINOI    :: ()  -> ()   -> DNA
-  HHIHOI    :: (DNA , DNA) -> DNA
+  EItherDNA :: ()  -> DNA -> DNA  -> DNA
+  HI        :: ()  -> DNA -> ()   -> DNA
+  OI        :: ()  -> ()  -> DNA  -> DNA
+  NI        :: ()  -> ()  -> ()   -> DNA
+  HHINOI    :: ()  -> DNA -> ()   -> DNA
+  HOINHI    :: ()  -> ()  -> DNA  -> DNA
+  NHINOI    :: ()  -> ()  -> ()   -> DNA
+  HHIHOI    :: ()  -> (DNA , DNA) -> DNA
   SPLIT     :: DNA -> (DNA , DNA) -> DNA
   
 
 instance Show DNA where
-  showsPrec d (EItherDNA h i)  = showsPrec d '(' . showsPrec d h . showsPrec d i . showsPrec d ')'
-  showsPrec d (HI _ i) = showsPrec d "I."
-  showsPrec d (OI _ o) = showsPrec d ".O"
-  showsPrec d (NI _ _) = showsPrec d ".."
-  showsPrec d (NHINOI _ _) = showsPrec d "XX"
+  showsPrec d (EItherDNA _ h i)  = showsPrec d '(' . showsPrec d h . showsPrec d i . showsPrec d ')'
+  showsPrec d (HI _ i _) = showsPrec d ".I."
+  showsPrec d (OI _ _ o) = showsPrec d "..O"
+  showsPrec d (NI _ _ _) = showsPrec d "..."
+  showsPrec d (NHINOI _ _ _) = showsPrec d "XXX"
+
+  showsPrec d (HHINOI _ x _ ) = showsPrec d '(' . showsPrec (d-1) x   . showsPrec d '?'   . showsPrec d ')'
+  showsPrec d (HOINHI _ _ y ) = showsPrec d '(' . showsPrec (d-1) '?' . showsPrec (d-1) y . showsPrec d ')'
+
+  showsPrec d (NHINOI _ _ _ )= showsPrec d '(' . showsPrec (d-1) '?' . showsPrec (d-1) '?' . showsPrec d ')'
+  showsPrec d (HHIHOI _ (x,y))= showsPrec d '(' . showsPrec (d-1) x . showsPrec (d-1) y . showsPrec d ')'
+  showsPrec d (xx `SPLIT` (xi,xo))=showsPrec d '(' . showsPrec (d-1) xx . showsPrec (d-1) xi . showsPrec (d-1) xo . showsPrec d ')'
+
+
+
+
+
+
 
 instance Enum DNA where
-  toEnum 0 = NHINOI    ()         ()
-  toEnum 1 = HHINOI    (toEnum 0) ()
-  toEnum 2 = HOINHI    ()         (toEnum 0)
-  toEnum 3 = EItherDNA (toEnum 0) (toEnum 0)
+  toEnum 0 = NHINOI    () ()         ()
+  toEnum 1 = HHINOI    () (toEnum 0) ()
+  toEnum 2 = HOINHI    () ()         (toEnum 0)
+  toEnum 3 = EItherDNA () (toEnum 0) (toEnum 0)
  
 data Nat where
   FromDNA :: DNA -> Nat
